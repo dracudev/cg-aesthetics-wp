a78a061eccc0286d74fd44dcd16426fe01d1ab8c
Javier Andreu <andreujavier99@gmail.com>
2025-11-12 21:09:09 +0100

    fix: add Netlify adapter to Astro configuration

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,12 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: netlify(),
+
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  integrations: [react()]
+  integrations: [react()],
 });
